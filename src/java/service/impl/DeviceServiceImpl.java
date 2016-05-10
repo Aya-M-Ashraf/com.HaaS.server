ed90fc0d23390cd1f187afd55aca809983c8a804
Ahmed Ashraf <Ahmed Ashraf@DESKTOP-JNGM71A>
2016-05-10 21:25:03 +0200

    After Adding UserServiceImpl and other modifications 2

---
+++ src/java/service/impl/DeviceServiceImpl.java
@@ -0,0 +1,17 @@
+package service.impl;
+
+import entity.Device;
+import service.interfaces.DeviceService;
+
+/**
+ *
+ * @author Aya M. Ashraf
+ */
+public class DeviceServiceImpl implements DeviceService {
+
+    @Override
+    public boolean addDevice(Device device) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
--- src/java/service/interfaces/DeviceService.java
+++ src/java/service/interfaces/DeviceService.java
@@ -1,14 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package service.interfaces;
+
+import entity.Device;
 
 /**
  *
  * @author Aya M. Ashraf
  */
 public interface DeviceService {
+    
+    public boolean addDevice(Device device);
     
 }
